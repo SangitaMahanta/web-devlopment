3d74bc12b19381653bb4a73832df700bd1396ff0
SangitaMahanta <sangitamiki1998@gmail.com>
2021-05-20 08:03:33 +0530

    function in js

---
+++ JS/index.js
@@ -0,0 +1,105 @@
+   // java script consol API
+//    console.log("hello world",6+4,"anotherl log");
+//    console.warn("warnning");
+//    console.error("this is error");
+   // document.write("welcome")
+
+//    javascript variable
+// container to store data value
+var number1 = 25;
+var number2 = 26;
+// console.log(number1+number2);
+ 
+// datatypes in javascript
+// num var
+var num1=255;
+var num2=555;
+// console.log(num1+num2);
+
+
+// string var
+var str="this is a string";
+var str1="this is  a another string";
+
+//  object
+var marks={
+    subham : 668,
+    miki:442,
+    harry:414
+}
+// console.log(marks);
+
+// booleans
+var a= true;
+var b= false;
+// console.log(a)
+
+//  datatypes
+// in javascript 2 types of datatypes are uses
+// 1.primitive datatypes : undefined,null,number,string,Boolean,symbole
+// 2.Referance datatypes : arrays and objects
+
+var arr=[1,"raja",2,"miki",45,6]
+// console.log(arr)
+
+
+// operator
+var m=6
+var n=44
+// console.log("value of m and n is ",m+n)
+
+// if else condition
+console.log('This is tutorial 8');
+const age = 128;
+const doesDrive = false;
+// const vari = 34;
+
+// if (age!=19){
+//     console.log('Age is not 19')
+// }
+
+// if(age !== 65){
+//     console.log('Age is not 65')
+// }
+
+// else{
+//     console.log('Age is not 19')
+// }
+
+// if (typeof vari !== 'undefined'){
+//     console.log('Vari is defined');
+// }
+
+// else{
+//     console.log('Vari is not defined');
+// }
+
+// if (doesDrive || age>18){
+//     console.log("You can drive");
+// }
+// else{
+//     console.log("You cannot drive");
+// }
+
+// console.log(age==45? 'Age is 45': 'Age is not 45');
+
+switch (age) {
+    case 18:
+        console.log("You are 18");
+        break;
+
+    case 28:
+        console.log("You are 28");
+        break;
+
+    case 38:
+        console.log("You are 38");
+        break;
+
+    default:
+        console.log("You are unknown age");
+        break;
+}         
+
+
+// function
